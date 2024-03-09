faa959bf8a07f744c4f0237b28cce2b18e63b305
Ricardo Vega <rvega@dataqu.cl>
2024-03-09 14:19:23 -0300

    Básico servicios

--- src/app/dbz/pages/main-page-component.ts
+++ src/app/dbz/pages/main-page-component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -7,35 +8,10 @@ import { Character } from '../interfaces/character.interface';
 })
 
 export class MainPageComponent implements OnInit {
-  public characters: Character[] = [
-    {
-      name: 'Krillin',
-      power: 1000
-    },
-    {
-      name: 'Goku',
-      power: 9500
-    },
-    {
-      name: 'Vegeta',
-      power: 7500
-    }
-  ];
 
-  constructor() { }
+
+  constructor(public dbzService:DbzService) { }
 
   ngOnInit() { }
-
-  onNewCharacter(character: Character):void {
-    let NewCharacter = {
-      name : character.name,
-      power : character.power
-    }
-    this.characters.push(NewCharacter);
-  }
-
-  onDeleteCharacter(index: number):void {
-    this.characters.splice(index,1);
-  }
 
 }
